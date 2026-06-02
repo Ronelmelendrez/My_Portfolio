14c927c605055987c1e87c246cd37fdd8b3f2a2f
Ronel <ronelmelendrez01@gmail.com>
2026-06-03 00:30:28 +0800

    feat: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "animation-vendor": ["framer-motion"],
+          "icon-vendor": ["react-icons"],
+        },
+      },
+    },
+  },
+});
