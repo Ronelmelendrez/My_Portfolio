90c7969507ef3d77bba4716da9cbfeb342343157
Ronel <ronelmelendrez01@gmail.com>
2026-06-03 23:59:45 +0800

    feat:  add  home tsx file in the section components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Hero from "../components/sections/Hero";
+import About from "../components/sections/About";
+import Skills from "../components/sections/Skills";
+import Experience from "../components/sections/Experience";
+import Projects from "../components/sections/Projects";
+import Services from "../components/sections/Services";
+import Testimonials from "../components/sections/Testimonials";
+import Contact from "../components/sections/Contact";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects featured={true} />
+      <Services />
+      <Testimonials />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
