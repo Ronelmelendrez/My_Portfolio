837b85b24f7cf97feac20c2e7623cbef239a88d0
Ronel <ronelmelendrez01@gmail.com>
2026-06-11 22:19:01 +0800

    refactor: change the font style to mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ export default {
       },
 
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
+         mono: ['"JetBrains Mono"', 'monospace'],
       },
 
       animation: {
